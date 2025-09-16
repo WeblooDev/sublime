b503fda840414d51c2d0facdc4e5398faaa3a40c
nabeff <neffina8@gmail.com>
2025-09-16 18:16:40 +0100

    v12

--- src/blocks/CatalogSection/Component.tsx
+++ src/blocks/CatalogSection/Component.tsx
@@ -17,6 +17,7 @@ export const CatalogSection: React.FC<CatalogSectionBlock> = ({
           resource={backgroundImage}
           imgClassName="w-full h-full min-h-[200px] object-cover max-h-[500px] "
         />
+        
 
         <div className="container mx-auto flex flex-col md:flex-row justify-between gap-4 items-start py-10">
           <h1 className="text-2xl md:text-4xl w-full md:w-[40%] ">{title}</h1>
