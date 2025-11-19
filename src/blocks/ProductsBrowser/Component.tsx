20190a90b62bccdf0bc7c57b90eeae9e95317229
nabeff <neffina8@gmail.com>
2025-11-19 13:12:27 +0100

    remove all

--- src/blocks/ProductsBrowser/Component.tsx
+++ src/blocks/ProductsBrowser/Component.tsx
@@ -54,8 +54,6 @@ export default async function ProductsBrowser(props: Props) {
   return (
     <section className="py-12 md:py-16">
       <div className="container mx-auto px-4 md:px-6">
-        <h2 className="text-2xl md:text-4xl font-semibold mb-6">{title}</h2>
-
         <ProductsBrowserClient
           showCategoryFilter={!!showCategoryFilter}
           pageSize={PAGE_SIZE}
