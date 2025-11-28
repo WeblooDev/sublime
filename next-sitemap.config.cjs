41abc544497424ad4979dd6b1e4c98f2041aee54
nabeff <neffina8@gmail.com>
2025-11-28 16:12:23 +0100

    robot.tsx

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,18 +1,26 @@
-const SITE_URL =
+// Normalise SITE_URL so it ALWAYS has https://
+const rawUrl =
   process.env.NEXT_PUBLIC_SERVER_URL ||
   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+  'https://www.sublimevital.com'
+
+const SITE_URL = rawUrl.startsWith('http') ? rawUrl : `https://${rawUrl}`
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: SITE_URL,
   generateRobotsTxt: true,
-  exclude: ['/posts-sitemap.xml', '/pages-sitemap.xml', '/*', '/posts/*'],
+
+  // keep your excludes if you really want them,
+  // but I've removed '/*' because it excludes everything.
+  exclude: ['/posts-sitemap.xml', '/pages-sitemap.xml', '/posts/*'],
+
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
-        disallow: '/admin/*',
+        disallow: ['/admin/'], // same as SEO guy
+        allow: '/', // explicitly allow everything else
       },
     ],
     additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`, `${SITE_URL}/posts-sitemap.xml`],
